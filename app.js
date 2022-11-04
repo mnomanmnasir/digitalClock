254ac95851ae90b3c2c28f1d9e2892efdbd9160e
Noman Muhammad Nasir <102538287+mnomanmnasir@users.noreply.github.com>
2022-11-04 10:50:20 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-var increment = 30;
-for (var i = 0; i <= 11; i++) {
-  var w = i * increment;
-  var hours = document.querySelectorAll(".hours");
-  var s = (hours[i].style.transform = "rotate(" + w + "deg)");
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+    
+    if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+    
+    setTimeout(showTime, 1000);
+    
 }
-var timing = setInterval(time, 1000);
-function time() {
-  var arms = document.querySelectorAll(".arms");
-  var date = new Date();
-  var hour = date.getHours() * 30;
-  var min = date.getMinutes() * 6;
-  var sec = date.getSeconds() * 6;
-  arms[0].style.transform = "rotateZ(" + hour + "deg)";
-  arms[1].style.transform = "rotateZ(" + min + "deg)";
-  arms[2].style.transform = "rotateZ(" + sec + "deg)";
-}
+
+showTime();
